1c516044292ecfa0cc5ead333207ccbc6321ff39
Freewind <pli@thoughtworks.com>
2014-10-18 01:04:42 +0800

    Try new acceptance style test, which is awesome

--- src/main/test/com/thoughtworks/pli/intellij/remotepair/actions/dialogs/ConnectServerFormSpec.scala
+++ src/main/test/com/thoughtworks/pli/intellij/remotepair/actions/dialogs/ConnectServerFormSpec.scala
@@ -1,23 +1,33 @@
 package com.thoughtworks.pli.intellij.remotepair.actions.dialogs
 
-import org.specs2.mutable.Specification
+import org.specs2._
 import org.specs2.specification.Scope
 
-class ConnectServerFormSpec extends Specification {
+class ConnectServerFormSpec extends Specification { def is = s2"""
 
-  "ConnectServerForm" should {
-    "be initialized with target server ip" in new Mocking {
-      form.init("1.1.1.1", 0, "any")
-      form.getHost === "1.1.1.1"
-    }
-    "be initialized with target server port" in new Mocking {
-      form.init("any", 123, "any")
-      form.getPort === "123"
-    }
-    "be initialized with client name" in new Mocking {
-      form.init("any", 0, "Freewind")
-      form.getClientName === "Freewind"
-    }
+ConnectServerForm can be initialized with some values,
+which will be displayed immediately when the dialog is opened,
+so user can type less and have a good default values
+
+e.g.
+
+  1. the target server ip $e1
+  2. the server port $e2
+  3. the client name $e3
+
+"""
+
+  def e1 = new Mocking {
+    form.init("1.1.1.1", 0, "any")
+    form.getHost === "1.1.1.1"
+  }
+  def e2 = new Mocking {
+    form.init("any", 123, "any")
+    form.getPort === "123"
+  }
+  def e3 = new Mocking {
+    form.init("any", 0, "Freewind")
+    form.getClientName === "Freewind"
   }
 
   trait Mocking extends Scope {
