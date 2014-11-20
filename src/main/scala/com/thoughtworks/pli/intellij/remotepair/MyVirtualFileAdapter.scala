b3c60721fde71f04de3229aeb2e0a8c3545ef1fb
Freewind <pli@thoughtworks.com>
2014-11-20 22:50:32 +1100

    Remove unused imports

--- src/main/scala/com/thoughtworks/pli/intellij/remotepair/MyVirtualFileAdapter.scala
+++ src/main/scala/com/thoughtworks/pli/intellij/remotepair/MyVirtualFileAdapter.scala
@@ -1,15 +1,6 @@
 package com.thoughtworks.pli.intellij.remotepair
 
-import com.intellij.openapi.fileEditor._
 import com.intellij.openapi.vfs._
-import org.jetbrains.annotations.NotNull
-import com.intellij.util.messages.MessageBusConnection
-import com.intellij.openapi.components.ProjectComponent
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.impl.BulkVirtualFileListenerAdapter
-import com.intellij.openapi.editor.event.{DocumentEvent, DocumentListener}
-import com.intellij.openapi.util.{UserDataHolder, Key}
-import com.intellij.openapi.editor.Editor
 
 object MyVirtualFileAdapter extends VirtualFileAdapter {
   override def fileDeleted(event: VirtualFileEvent) {
