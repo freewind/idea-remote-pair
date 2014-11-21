07b7eaff452af52403520da703543383e168866b
Freewind <pli@thoughtworks.com>
2014-11-21 14:57:58 +1100

    Remove the ip getter which cause errors

--- src/main/scala/com/thoughtworks/pli/intellij/remotepair/server/StandaloneServer.scala
+++ src/main/scala/com/thoughtworks/pli/intellij/remotepair/server/StandaloneServer.scala
@@ -1,11 +1,9 @@
 package com.thoughtworks.pli.intellij.remotepair.server
 
-import java.net.InetAddress
-
 object StandaloneServer extends App {
 
   val port = 8888
   (new Server).start(port)
-  println("######### server should be started on " + InetAddress.getLocalHost() + ":" + port)
+  println("######### server should be started on: " + port)
 
 }
