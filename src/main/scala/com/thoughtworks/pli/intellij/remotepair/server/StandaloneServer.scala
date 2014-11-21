e9fa3b76a44ae1516c5dfbf6a4304b403fc57fdf
Freewind <pli@thoughtworks.com>
2014-11-21 15:57:26 +1100

    Turn to 8888 again

--- src/main/scala/com/thoughtworks/pli/intellij/remotepair/server/StandaloneServer.scala
+++ src/main/scala/com/thoughtworks/pli/intellij/remotepair/server/StandaloneServer.scala
@@ -2,7 +2,7 @@ package com.thoughtworks.pli.intellij.remotepair.server
 
 object StandaloneServer extends App {
 
-  val port = 9090
+  val port = 8888
   (new Server).start(port)
   println("######### server should be started on: " + port)
 
