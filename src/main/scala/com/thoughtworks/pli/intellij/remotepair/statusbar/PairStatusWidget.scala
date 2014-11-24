cc02b3055c3f348103c70bc5e846de0c2387b658
Freewind <pli@thoughtworks.com>
2014-11-24 23:10:17 +0800

    The status bar widget can popup a list of values now

--- src/main/scala/com/thoughtworks/pli/intellij/remotepair/statusbar/PairStatusWidget.scala
+++ src/main/scala/com/thoughtworks/pli/intellij/remotepair/statusbar/PairStatusWidget.scala
@@ -1,17 +1,22 @@
 package com.thoughtworks.pli.intellij.remotepair.statusbar
 
+import java.awt.Component
 import java.awt.event.MouseEvent
-import javax.swing.Icon
 
-import com.intellij.icons.AllIcons
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, DataContext, DefaultActionGroup}
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.wm.StatusBarWidget.{IconPresentation, PlatformType}
+import com.intellij.openapi.ui.popup.{JBPopupFactory, ListPopup}
+import com.intellij.openapi.wm.StatusBarWidget.{MultipleTextValuesPresentation, PlatformType}
 import com.intellij.openapi.wm.{StatusBar, StatusBarWidget}
 import com.intellij.util.Consumer
+import com.thoughtworks.pli.intellij.remotepair.statusbar.PairStatusWidget.{NotConnect, PairStatus}
 
-class PairStatusWidget(var project: Project) extends StatusBarWidget with IconPresentation {
+class PairStatusWidget(var project: Project) extends StatusBarWidget with MultipleTextValuesPresentation {
 
   private var statusBar: StatusBar = _
+
+  private var myText = "start-value"
 
   override def ID() = classOf[PairStatusWidget].getName
   override def install(statusBar: StatusBar): Unit = this.statusBar = statusBar
@@ -22,7 +27,27 @@ class PairStatusWidget(var project: Project) extends StatusBarWidget with IconPr
   }
 
 
-  override def getIcon: Icon = AllIcons.Ide.Warning_notifications
+  var currentStatus: PairStatus = NotConnect
+
+  override def getPopupStep: ListPopup = {
+    val group = createActionGroup()
+    val dataContext: DataContext = DataManager.getInstance.getDataContext(statusBar.asInstanceOf[Component])
+    JBPopupFactory.getInstance.createActionGroupPopup("PPPppppppppair", group, dataContext, null, false)
+  }
+
+  private def createAction(label: String) = {
+    new AnAction(label) {
+      override def actionPerformed(anActionEvent: AnActionEvent): Unit = println("Clicked " + label)
+    }
+  }
+
+  private def createActionGroup(): DefaultActionGroup = {
+    new DefaultActionGroup(createAction("action1"), createAction("action2"))
+  }
+
+  override def getMaxValue: String = myText
+  override def getSelectedValue: String = myText
+
   override def getTooltipText: String = "This is the pair status widget"
   override def getClickConsumer: Consumer[MouseEvent] = new Consumer[MouseEvent] {
     override def consume(t: MouseEvent): Unit = {
@@ -30,3 +55,16 @@ class PairStatusWidget(var project: Project) extends StatusBarWidget with IconPr
     }
   }
 }
+
+object PairStatusWidget {
+
+  sealed abstract class PairStatus(val icon: String)
+
+  case object NotConnect extends PairStatus("-x-")
+
+  case object CaretSharingMode extends PairStatus("o-o")
+
+  case object ParallelMode extends PairStatus("o|o")
+
+}
+
