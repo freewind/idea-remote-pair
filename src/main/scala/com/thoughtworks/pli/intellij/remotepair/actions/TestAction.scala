f0bdd078b115cc824d20d1495c3ba21ab28e799d
Freewind <pli@thoughtworks.com>
2014-11-28 11:43:14 +0800

    Add a test action for convenience

---
+++ src/main/scala/com/thoughtworks/pli/intellij/remotepair/actions/TestAction.scala
@@ -0,0 +1,8 @@
+package com.thoughtworks.pli.intellij.remotepair.actions
+
+import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
+
+class TestAction extends AnAction {
+  override def actionPerformed(event: AnActionEvent): Unit = {
+  }
+}
