c2012ac0fb972c2a82f6696c1bee96c719560851
Peng.Li <peng.li@rea-group.com>
2015-03-08 18:56:16 +0800

    Try to use IDEA provided test base, but failed
    In the fake editor provided by the test base class, it won't publish events when content changes

--- src/test/scala/com/thoughtworks/pli/remotepair/idea/MyIdeaTest.scala
+++ src/test/scala/com/thoughtworks/pli/remotepair/idea/MyIdeaTest.scala
@@ -1,7 +1,11 @@
 package com.thoughtworks.pli.remotepair.idea
 
 import com.intellij.openapi.editor.EditorModificationUtil
-import com.intellij.testFramework.LightPlatformCodeInsightTestCase
+import com.intellij.testFramework.{LightPlatformCodeInsightTestCase, LightPlatformTestCase}
+import com.thoughtworks.pli.intellij.remotepair.protocol.CreateProjectRequest
+import com.thoughtworks.pli.intellij.remotepair.server.Server
+import com.thoughtworks.pli.remotepair.idea.core._
+import io.netty.channel.{ChannelHandlerAdapter, ChannelHandlerContext}
 
 /*
 When run the test within idea, add following command line (sample here):
@@ -17,9 +21,68 @@ When run the test within idea, add following command line (sample here):
 -Didea.test.group=ALL_EXCLUDE_DEFINED
 -Didea.platform.prefix=Idea
  */
-class MyIdeaTest extends LightPlatformCodeInsightTestCase {
+class MyIdeaTest extends LightPlatformCodeInsightTestCase with InvokeLater with CurrentProjectHolder with PublishEvents {
 
-  import LightPlatformCodeInsightTestCase._
+  private var _currentProject: RichProject = _
+
+  override def currentProject: RichProject = _currentProject
+
+
+  import com.intellij.testFramework.LightPlatformCodeInsightTestCase._
+  import com.intellij.testFramework.LightPlatformTestCase._
+
+  private val port = 18888
+
+  private var server: Option[Server] = None
+
+  private var handler2: ChannelHandlerContext = _
+
+  override def setUp(): Unit = {
+    super.setUp()
+    _currentProject = Projects.init(getProject)
+
+    startTheServer()
+
+    Thread.sleep(5000)
+
+    val handler = new MyChannelHandler(currentProject) {
+      override def channelActive(ctx: ChannelHandlerContext): Unit = {
+        super.channelActive(ctx)
+        publishEvent(CreateProjectRequest("ttt", "client111"))
+      }
+      override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = {
+        super.channelRead(ctx, msg)
+        println("############ client1 msg read: " + msg)
+      }
+    }
+
+    new Client(ServerAddress("localhost", port)).connect(handler)
+
+
+    new Client(ServerAddress("localhost", port)).connect(new ChannelHandlerAdapter() {
+
+      override def channelActive(ctx: ChannelHandlerContext): Unit = {
+        handler2 = ctx
+        handler2.writeAndFlush(CreateProjectRequest("ttt", "client222").toMessage)
+      }
+
+      override def channelRead(ctx: ChannelHandlerContext, msg: scala.Any): Unit = {
+        println("************ client2 msg read: " + msg)
+      }
+
+    })
+
+    Thread.sleep(5000)
+  }
+
+  override def tearDown(): Unit = {
+    Thread.sleep(20000)
+    server.foreach(_.close())
+    server = None
+
+    _currentProject = null
+    super.tearDown()
+  }
 
   def testInsertStringAtCaretNotMovingCaret(): Unit = {
     val fileName: String = getTestName(false) + ".txt"
@@ -29,4 +92,14 @@ class MyIdeaTest extends LightPlatformCodeInsightTestCase {
     checkResultByText("text <caret>xx")
   }
 
+  private def startTheServer(): Unit = {
+    val thread = new Thread(new Runnable {
+      override def run(): Unit = {
+        server = Some(new Server(host = None, port))
+        server.foreach(_.start())
+      }
+    })
+    thread.setDaemon(true)
+    thread.start()
+  }
 }
