e444ef229889d099c7c6777aee611db620490558
Peng.Li <peng.li@rea-group.com>
2015-03-14 19:34:30 +0800

    Add logging for read or write messages

--- src/test/scala/com/thoughtworks/pli/remotepair/idea/core/RichProjectSpec.scala
+++ src/test/scala/com/thoughtworks/pli/remotepair/idea/core/RichProjectSpec.scala
@@ -1,16 +1,18 @@
 package com.thoughtworks.pli.remotepair.idea.core
 
 import com.intellij.openapi.project.Project
+import com.thoughtworks.pli.remotepair.idea.MocksModule
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-class RichProjectSpec extends Specification with Mockito {
+class RichProjectSpec extends Specification with Mockito with MocksModule {
+  override lazy val richProjectFactory: RichProjectFactory = wire[RichProjectFactory]
 
   "2 rich projects" should {
     "equal with each other if the raw projects are equal" in {
       val raw = mock[Project]
-      val project1 = new RichProject(raw)
-      val project2 = new RichProject(raw)
+      val project1 = richProjectFactory.create(raw)
+      val project2 = richProjectFactory.create(raw)
       project1 === project2
     }
   }
