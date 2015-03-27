c8a5925eeade204ba1c7b6d2a5d9c78c6f469b00
Peng.Li <peng.li@rea-group.com>
2015-03-27 10:47:12 +0800

    Add test for HandleCreateServerDocument

--- src/main/scala/com/thoughtworks/pli/remotepair/idea/event_handlers/HandleCreateServerDocumentRequest.scala
+++ src/main/scala/com/thoughtworks/pli/remotepair/idea/event_handlers/HandleCreateServerDocumentRequest.scala
@@ -5,6 +5,7 @@ import com.thoughtworks.pli.remotepair.idea.core.{GetFileContent, GetFileByRelat
 import com.thoughtworks.pli.remotepair.idea.utils.RunReadAction
 
 case class HandleCreateServerDocumentRequest(runReadAction: RunReadAction, publishEvent: PublishEvent, getFileByRelative: GetFileByRelative, getFileContent: GetFileContent) {
+
   def apply(request: CreateServerDocumentRequest): Unit = runReadAction {
     getFileByRelative(request.path).map(getFileContent.apply).foreach { content =>
       publishEvent(CreateDocument(request.path, content))
---
+++ src/test/scala/com/thoughtworks/pli/remotepair/idea/event_handlers/HandleCreateServerDocumentRequestSpec.scala
@@ -0,0 +1,33 @@
+package com.thoughtworks.pli.remotepair.idea.event_handlers
+
+import com.intellij.openapi.vfs.VirtualFile
+import com.thoughtworks.pli.intellij.remotepair.protocol.{CreateDocument, Content, CreateServerDocumentRequest}
+import com.thoughtworks.pli.remotepair.idea.MocksModule
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+
+class HandleCreateServerDocumentRequestSpec extends Specification with Mockito with MocksModule {
+  isolated
+
+  override lazy val handleCreateServerDocumentRequest = new HandleCreateServerDocumentRequest(runReadAction, publishEvent, getFileByRelative, getFileContent)
+
+  val file = mock[VirtualFile]
+
+  getFileByRelative.apply("/abc") returns Some(file)
+  getFileContent.apply(file) returns Content("hello world", "UTF-8")
+
+  val request = CreateServerDocumentRequest("/abc")
+
+  "When client receives CreateServerDocumentRequest, it" should {
+    "public CreateDocument event to server" in {
+      handleCreateServerDocumentRequest(request)
+      there was one(publishEvent).apply(CreateDocument("/abc", Content("hello world", "UTF-8")))
+    }
+    "not public CreateDocument event to server if can't find corresponding file" in {
+      getFileByRelative.apply("/abc") returns None
+      handleCreateServerDocumentRequest(request)
+      there was no(publishEvent).apply(CreateDocument("/abc", Content("hello world", "UTF-8")))
+    }
+  }
+
+}
