8e728500a766658ce1bc20cd3e22f08224d9fe01
Peng.Li <nowind_lee@qq.com>
2015-10-03 13:37:33 +0800

    Some more app action

--- src/main/scala/com/thoughtworks/pli/remotepair/effects/AppAction.scala
+++ src/main/scala/com/thoughtworks/pli/remotepair/effects/AppAction.scala
@@ -1,5 +1,9 @@
 package com.thoughtworks.pli.remotepair.effects
 
+import com.thoughtworks.pli.remotepair.core.models.MyProject
+
 sealed trait AppAction[A]
 
 case class LogInfo(message: String) extends AppAction[Unit]
+
+case object GetCurrentProject extends AppAction[MyProject]
--- src/main/scala/com/thoughtworks/pli/remotepair/effects/Script.scala
+++ src/main/scala/com/thoughtworks/pli/remotepair/effects/Script.scala
@@ -1,9 +1,12 @@
 package com.thoughtworks.pli.remotepair.effects
 
+import com.thoughtworks.pli.remotepair.core.models.MyProject
+
 import scalaz._
 
 object Script {
 
+  def getCurrentProject: Script[MyProject] = toScript(GetCurrentProject)
   def logInfo(message: String): Script[Unit] = toScript(LogInfo(message))
   def pure[A](a: A): Script[A] = Monad[Script].pure(a)
 
